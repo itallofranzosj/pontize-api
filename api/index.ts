db32e430cc61fec7c0297d57d5bf2f2da3d7b8a9
Itallo Franzo <itallofranzo@fitoformulalab.com.br>
2026-06-16 09:46:17 -0300

    fix: Remove serverless config, use Node.js server

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../src/api/index";
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  return app.fetch(req, {
-    waitUntil: (promise: Promise<any>) => promise,
-  });
-};
